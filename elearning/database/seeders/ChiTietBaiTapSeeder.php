22b1e3fb6223064b9f861919f56ad830ced3d918
VietTinh <0306191473@caothang.edu.vn>
2021-12-22 15:24:19 +0700

    elearning

---
+++ elearning/database/seeders/ChiTietBaiTapSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class ChiTietBaiTapSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
